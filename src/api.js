a4d67d041bd9eb9753a3867541613e10b5b9c3db
tmusinov <102414199+tmusinov@users.noreply.github.com>
2022-08-25 15:18:35 +0300

    Search using GeoDB

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4ac863b417msh7f523ce66090c8ap132aebjsn239389a07791',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+
