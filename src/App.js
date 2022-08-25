047ced36a6c9d8c89071c58479d0da6c3323cfeb
tmusinov <102414199+tmusinov@users.noreply.github.com>
2022-08-25 15:47:17 +0300

    Static weather component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Search from './components/search/search';
+import CurrentWeather from './components/search/current-weather/current-weather';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
     </div>
   );
 }
